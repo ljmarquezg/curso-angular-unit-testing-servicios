74de2698498824a181a3a5bd93b9b938083e0160
Luis Marquez <ljmarquezg@gmail.com>
2024-11-10 14:29:39 +0100

    08 - products service

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  private productsService = inject(ProductsService);
+  products: WritableSignal<Product[]> = signal([]);
+
+  ngOnInit() {
+    this.productsService.getAllSimple().subscribe(products => {
+      this.products.set(products)
+    });
+  }
 
 }
