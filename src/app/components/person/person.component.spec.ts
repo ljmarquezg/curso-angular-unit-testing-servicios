61fe9f1d8b3a2a7a58294afda68c040b2d5a6c57
Luis Marquez <ljmarquezg@gmail.com>
2024-11-12 20:19:08 +0100

    21 - debugElement

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PersonComponent } from './person.component';
 
@@ -23,9 +25,23 @@ describe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a <p> with "Soy un párrafo"', () => {
+  it('should have a <p> with "Soy un párrafo" with nativeElement', () => {
     const personElement: HTMLElement = fixture.nativeElement;
     const p = personElement.querySelector('p');
     expect(p?.textContent).toEqual('Soy un párrafo');
+  });
+
+  it('should have a <p> with "Soy un párrafo" with debutElement', () => {
+    const personDebugElement: DebugElement = fixture.debugElement;
+    const personElement: HTMLElement = personDebugElement.nativeElement;
+    const p = personElement.querySelector('p');
+    expect(p?.textContent).toEqual('Soy un párrafo');
+  });
+
+  it('should have a <h3> with "Person Component" with debutElement query', () => {
+    const personDebugElement: DebugElement = fixture.debugElement;
+    const pDebug: DebugElement = personDebugElement.query(By.css('h3'));
+    const personElement: HTMLElement = pDebug.nativeElement;
+    expect(personElement?.textContent).toEqual('Person Component');
   });
 });
